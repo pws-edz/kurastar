2d80f89798ce679c1bbba050cc5bdd031a16e00b
sumaaar <philwebservices.pwsojt4@gmail.com>
2015-09-11 13:43:12 +0800

    wp-config

--- wp-config.php
+++ wp-config.php
@@ -16,13 +16,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'kurstar_db');
+define('DB_NAME', 'kurastar_db');
 
 /** MySQL database username */
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password');
+define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
