e9d1623605b4eace249ed7d914b89b1945015c4f
Claudine Javellana <philwebservices.claudine@gmail.com>
2015-10-28 13:43:53 +0800

    edited single page

--- wp-content/themes/kurastar/single.php
+++ wp-content/themes/kurastar/single.php
@@ -26,17 +26,6 @@ get_header(); ?>
 			if ( comments_open() || get_comments_number() ) :
 				comments_template();
 			endif;
-
-			// Previous/next post navigation.
-			the_post_navigation( array(
-				'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'twentyfifteen' ) . '</span> ' .
-					'<span class="screen-reader-text">' . __( 'Next post:', 'twentyfifteen' ) . '</span> ' .
-					'<span class="post-title">%title</span>',
-				'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'twentyfifteen' ) . '</span> ' .
-					'<span class="screen-reader-text">' . __( 'Previous post:', 'twentyfifteen' ) . '</span> ' .
-					'<span class="post-title">%title</span>',
-			) );
-
 		// End the loop.
 		endwhile;
 		?>
